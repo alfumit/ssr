3e6b3f7ed919efb136d1f593cdcb2fc163be7cee
Alex Fedotov <alfumit@gmail.com>
2018-07-18 23:46:07 +0300

    Fighting the nguniversal -> angular cli v6 incompatibility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
+    BrowserModule.withServerTransition({appId: 'ssr'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
